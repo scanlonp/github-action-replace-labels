668150f196212995693357577bd7fc03a93ba8a9
Parker Scanlon <69879391+scanlonp@users.noreply.github.com>
2023-09-14 17:04:49 -0700

    add basic inputs and octokit command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
-  console.log('hi');
+  const token: string = core.getInput('github-token');
+  const labelToReplace: string = core.getInput('label-to-replace');
+  const labelToReplaceWith: string = core.getInput('label-to-replace-with');
+
+  console.log(labelToReplace, labelToReplaceWith);
+
+  const octokit = github.getOctokit(token);
+  const repo = github.context.repo;
+
+  await updateLabel(labelToReplace, labelToReplaceWith);
+
+  async function updateLabel(label: string, newLabel: string) {
+    console.log(`Updating label ${label} to ${newLabel}`);
+    await octokit.rest.issues.updateLabel({
+      owner: repo.owner,
+      repo: repo.repo,
+      name: label,
+      new_name: newLabel,
+    });
+  }
 }
+
+/**
+ * Renders a TypeScript list based on what we expect the list to look like in yaml.
+ * We expect to see something like "[item1,item2]". GitHub will return '' if the
+ * input is not defined, so treating the empty string like undefined.
+ */
+/*
+function renderListInput(rawInput: string): string[] {
+  return (rawInput === '' || rawInput === '[]') ? [] : rawInput.slice(1, -1).split(',');
+}
+*/
+
 
 run().catch(error => {
   core.setFailed(error.message);
